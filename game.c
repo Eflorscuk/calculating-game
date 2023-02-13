3e7a0f873264f156d103719b94b8943e52d1eb7e
Evandro Florscuk <evandro_ff@me.com>
2023-02-13 17:48:30 -0300

    Structuring our project

--- game.c
+++ game.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int difficulty;
+    int value1;
+    int value2;
+    int operation;
+    int result;
+} calculate;
+
+void start(); // jogar
+void showInfo(calculate calc); // mostrar info
+
+int points = 0; // Placar
 
 int main() {
+    // Must be called only once
+    srand(time(NULL));
+
+    start();
+
     return 0;
+}
+
+void start() {
+    // let's implement
+}
+
+void showInfo(calculate calc){
+    // let's implement
+}
+
+int add(int result, calculate calc){
+    // let's implement
+}
+
+int sub(int result, calculate calc){
+    // let's implement
+}
+
+int mul(int result, calculate calc){
+    // let's implement
 }
